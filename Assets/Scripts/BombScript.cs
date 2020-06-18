d67ced63fa707219de7bea452071d57ea6bfb2c4
S1r0 <vlad.kopanov01@mail.ru>
2020-06-18 15:48:07 +0300

    first commit

---
+++ Assets/Scripts/BombScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombScript : MonoBehaviour
+{
+    //public GameObject bomb;
+    private float timer = 5F;
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer<=0)
+        {
+            anim.Play("Expl");
+        }
+        else
+            anim.Play("Bomb_On");
+    }
+
+    public void DestroyBomb()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject bomb;
+    private int Force = 500;
+    List<GameObject> bombs = new List<GameObject>();
+
+    Animator animator;
+    Rigidbody2D rb;
+
+    [SerializeField]
+    int speed;
+
+    [SerializeField]
+    int jumpforce;
+
+    [SerializeField]
+    Transform groundCheck;
+
+    bool isGrounded = true;
+
+    private bool isFacingRight = true;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Physics2D.Linecast(new Vector2(transform.position.x -7, transform.position.y), groundCheck.position, 1 << LayerMask.NameToLayer("Ground")))
+        {
+            isGrounded = true;
+        }
+        else isGrounded = false;
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (!isFacingRight)
+                Flip();
+
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+            if(isGrounded)
+                animator.Play("Player_Run");
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            if(isFacingRight)
+            {
+                Flip();
+                isFacingRight = false;
+            }
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+            if (isGrounded)
+                animator.Play("Player_Run");
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+            animator.Play("Player_Idle");
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            rb.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+            animator.Play("Player_Jump");
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Instantiate(bomb, transform.position, transform.rotation);
+           // bomb.GetComponent<Rigidbody2D>().AddForce(new Vector2(1000, 1000), ForceMode2D.Impulse);
+        }
+
+
+    }
+    /// <summary>
+    /// Довнерский код
+    /// </summary>
+    private void Flip()
+    {
+        //меняем направление движения персонажа
+        isFacingRight = !isFacingRight;
+        //получаем размеры персонажа
+        Vector3 theScale = transform.localScale;
+        //зеркально отражаем персонажа по оси Х
+        theScale.x *= -1;
+        //задаем новый размер персонажа, равный старому, но зеркально отраженный
+        transform.localScale = theScale;
+    }
+}
